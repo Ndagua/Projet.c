d56134f84bf7bdd69b15a60af46b33a69d744ec5
Ndagua <diabymadicke9@gmail.com>
2026-02-18 14:29:18 +0000

    nombre pair

---
+++ Projet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+int x,n,i,somme=0,cpt=0,somme1=0,cpt1=0;
+float moy,moyi;
+do{
+    printf("Saisir n entier:");
+    scanf("%d",&n);
+    }while(n<0);
+    for(i=0;i<n;i++){
+        do{
+            puts("Veuillez saisir un entier");
+            scanf("%d",&x);
+        }while(x>0);
+        if(x%2==0){
+        	somme=somme+x;
+        	cpt++;
+		}
+		
+    }
+    moy=somme/cpt;
+    printf("La moyenne des nombres pairs est : %f",moy);
+return 0;
+}
